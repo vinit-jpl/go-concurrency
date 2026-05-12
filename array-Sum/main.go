bcec44445eca119b97c2787148be7e1d7fafeecd
vinit-jpl <vinit.jpl@gmail.com>
2026-05-12 23:58:57 +0530

    Refactor concurrent sum implementation to improve batch processing and enhance readability

--- array-Sum/main.go
+++ array-Sum/main.go
@@ -2,42 +2,60 @@ package main
 
 import "fmt"
 
-func Sum(s []int, c chan int) {
+const (
+	BATCH_SIZE = 5
+)
+
+func sumWorker(s []int, ch chan int) {
+
+	sum := 0
+
+	for _, v := range s {
+		sum += v
+	}
+
+	ch <- sum
+
+}
+func checkSum(s []int) {
 	sum := 0
 	for _, v := range s {
 		sum += v
 	}
 
-	c <- sum
-}
-func ChannelSum(s []int, batchCount int) int {
-
-	c := make(chan int)
-
-	numOfSumBatch := len(s) / batchCount
-
-	for i := 0; i < numOfSumBatch; i++ {
-		go Sum(s[i*batchCount:(i+1)*batchCount], c)
-
-	}
-
-	chanSum := 0
-
-	for i := 0; i < numOfSumBatch; i++ {
-		fmt.Printf("chan sum is %d \n", chanSum)
-		chanSum += <-c
-	}
-
-	return chanSum
-
+	fmt.Println("Normal sum:", sum)
 }
 
+func concurrentSum(s []int, ch chan int) {
+
+	numberOfBatches := (len(s) + BATCH_SIZE - 1) / BATCH_SIZE
+	for i := 0; i < len(s); i += BATCH_SIZE {
+		end := i + BATCH_SIZE
+
+		// prevent out of bound error
+		if end > len(s) {
+			end = len(s)
+		}
+
+		go sumWorker(s[i:end], ch)
+	}
+
+	res := 0
+
+	for i := 0; i < numberOfBatches; i++ {
+		res += <-ch
+	}
+
+	fmt.Println("Concurrent sum: ", res)
+
+}
 func main() {
 
-	const BATCH_COUNT = 5
+	s := []int{438, 6849, 4501, 7028, 5351, 6414, 2240, 9939, 3352, 2425, 2418, 2966, 2714, 6533, 164, 7407, 106, 7016, 3810, 9490, 2448, 7072, 9441, 388, 1063, 6914, 5961}
+	ch := make(chan int)
 
-	s := []int{1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393}
-	fmt.Println(len(s))
-	fmt.Println("Sum from channel: ", ChannelSum(s, BATCH_COUNT))
+	concurrentSum(s, ch)
+
+	checkSum(s)
 
 }
